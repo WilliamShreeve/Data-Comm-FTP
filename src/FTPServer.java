f3279ed60b4e2158a0c42901aed8b21393270d7d
William Shreeve <shreevewilliam@outlook.com>
2019-10-12 12:16:15 -0400

    Starting commit. Reformatted files to be under Source directory (src) so they play well with most IDEs.

---
+++ src/FTPServer.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class FTPServer {
+
+    public static void main(String argv[]) throws IOException {
+
+        String fromClient;
+        String clientCommand;
+        byte[] data;
+
+
+        ServerSocket welcomeSocket = new ServerSocket(12000);
+        String frstln;
+
+        while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+
+
+            DataOutputStream outToClient = new DataOutputStream(
+                    connectionSocket.getOutputStream());
+            BufferedReader inFromClient = new BufferedReader(
+                    new InputStreamReader(connectionSocket.getInputStream()));
+
+            fromClient = inFromClient.readLine();
+
+            StringTokenizer tokens = new StringTokenizer(fromClient);
+            frstln = tokens.nextToken();
+            port = Integer.parseInt(frstln);
+            clientCommand = tokens.nextToken();
+
+            if (clientCommand.equals("list:")) {
+
+                Socket dataSocket = new Socket(
+                        connectionSocket.getInetAddress(), port);
+                DataOutputStream dataOutToClient = new DataOutputStream(
+                        dataSocket.getOutputStream());
+
+
+            }
+
+            dataSocket.close();
+            System.out.println("Data Socket closed");
+        }
+
+        //......................
+
+        if (clientCommand.equals("retr:")) {
+            //..............................
+            //..............................
+        }
+    }
+}
+    
